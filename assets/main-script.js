dd0e111aa77e241fabe5bd60d7759fbe432d1530
Ash <acst52@gmail.com>
2022-12-15 13:55:17 -0500

    added some code, not functional yet

--- assets/main-script.js
+++ assets/main-script.js
@@ -65,3 +65,63 @@ let questions = [ {
         // - let initials = value that user inputted for initials, 
         // - then save score as an obj where score = time remaining + initials... then set the obj to local storage
         // - new js doc linked to highscores.html that pulls the info out of local storage & renders it on that page.
+
+
+
+
+
+
+            // *** UNDER CONSTRUCTION ***
+
+// let quizTime = 60;  // 10 seconds are supposed to be deducted for wrong answers...
+// let count = 0; // where it starts
+
+// function scoreRender(){
+//     if(score <= quizTime){
+//         counter.innerHTML = count;   
+//         count++;
+//     }else{
+//         count -10; 
+//         answerIsWrong();
+//     // now we need to move to the next Q, but first, we have to make sure there are Q's left in the quiz!
+//     if(runningQuestionIndex < lastQuestionIndex){
+//         runningQuestionIndex++;  // if above if statement is true, and there are Q's left, then we move to next Q:
+//         questionRender();
+//     }else{
+//         clearInterval(timer);  // if there are no q's left, then we clear the timer and show the user their score:
+//         scoreRender();
+//     }
+//     }
+// }
+
+
+// Add event listener to listen for submit event
+// document.querySelector('.answers').addEventListener('submit', function(e) {
+//     e.preventDefault(); // Prevent default form submission
+        
+// Initialize score
+// var score = 0;
+        
+// Check answers and update score
+// if (document.querySelector('input[name="q1"]:checked').value === 'a') {
+//     score++;
+//     }
+//     if (document.querySelector('input[name="q2"]:checked').value === 'a') {
+//         score++;
+//     }
+        
+// Show score to user
+    // alert('Your score: ' + score);
+    //  });
+
+
+//   to get user's high score to display: 
+
+  // Set the user's high score in local storage
+// localStorage.setItem('highScore', 10000000000000000);
+
+// Retrieve the user's high score from local storage
+// var highScore = localStorage.getItem('highScore');
+
+// Format the high score and display it on the page
+// document.getElementById('highScore').innerHTML = "Your high score: " + highScore;
